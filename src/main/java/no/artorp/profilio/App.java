ade2c58c7eb2303f1b030259a65161ea8e7f0086
Artorp <Artorp@users.noreply.github.com>
2017-03-30 19:11:48 +0200

    Add default uncaught exception handler
    Any uncaught exceptions are logged and then showed to the user in a
    dialog, if a non-deamon thread (subject to change) throws an uncaught
    exception, the platform shuts down

--- src/main/java/no/artorp/profilio/App.java
+++ src/main/java/no/artorp/profilio/App.java
@@ -5,11 +5,14 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import no.artorp.profilio.javafx.ExceptionDialog;
 import no.artorp.profilio.javafx.MainWindowController;
 import no.artorp.profilio.javafx.Registry;
 import no.artorp.profilio.logging.MyLogger;
@@ -50,44 +53,52 @@ public class App extends Application {
 				System.getProperty("javafx.version"),
 				System.getProperty("os.name"),
 				System.getProperty("os.arch")));
-
-		primaryStage.getIcons().add(new Image( getClass().getResourceAsStream("/icon_16.png")));
-		primaryStage.getIcons().add(new Image( getClass().getResourceAsStream("/icon_32.png")));
-		primaryStage.getIcons().add(new Image( getClass().getResourceAsStream("/icon_48.png")));
-		primaryStage.getIcons().add(new Image( getClass().getResourceAsStream("/icon_256.png")));
 		
-		Registry myRegistry = new Registry();
-		this.myRegistry = myRegistry;
+		Thread.setDefaultUncaughtExceptionHandler((t, e) -> Platform.runLater(() -> showUncaughtException(t, e)));
+		Thread.currentThread().setUncaughtExceptionHandler(this::showUncaughtException);
 		
-		// Set up SettingsIO object
-		String fileName = "settings.json";
-		File configDir = FileLocations.getConfigDirectory().toFile();
-		if (!configDir.exists()) {
-			if (!configDir.mkdirs()) {
-				String errorMsg = "Could not create config directory at location:\n"+configDir.getAbsolutePath();
-				LOGGER.severe(errorMsg);
-				throw new RuntimeException(errorMsg);
+		try {
+			primaryStage.getIcons().add(new Image( getClass().getResourceAsStream("/icon_16.png")));
+			primaryStage.getIcons().add(new Image( getClass().getResourceAsStream("/icon_32.png")));
+			primaryStage.getIcons().add(new Image( getClass().getResourceAsStream("/icon_48.png")));
+			primaryStage.getIcons().add(new Image( getClass().getResourceAsStream("/icon_256.png")));
+			
+			Registry myRegistry = new Registry();
+			this.myRegistry = myRegistry;
+			
+			// Set up SettingsIO object
+			String fileName = "settings.json";
+			File configDir = FileLocations.getConfigDirectory().toFile();
+			if (!configDir.exists()) {
+				if (!configDir.mkdirs()) {
+					String errorMsg = "Could not create config directory at location:\n"+configDir.getAbsolutePath();
+					LOGGER.severe(errorMsg);
+					throw new RuntimeException(errorMsg);
+				}
 			}
+			
+			File settingsFile = new File(configDir + File.separator + fileName);
+			FileIO fileIO = new FileIO();
+			SettingsIO settingsIO = new SettingsIO(settingsFile);
+			this.settingsIO = settingsIO;
+			
+			// Setup scene, controller, stage
+			primaryStage.setTitle("Profilio - Factorio Profile Manager");
+			
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainWindow.fxml"));
+			MainWindowController controller = new MainWindowController(primaryStage, settingsIO, fileIO, myRegistry);
+			loader.setController(controller);
+			BorderPane root = (BorderPane) loader.load();
+			
+			Scene scene = new Scene(root);
+			//scene.getStylesheets().add("/MainWindowStyle.css");
+			primaryStage.setScene(scene);
+			
+			primaryStage.show();
+		} catch (Throwable t) {
+			showUncaughtException(Thread.currentThread(), t);
+			throw t;
 		}
-		
-		File settingsFile = new File(configDir + File.separator + fileName);
-		FileIO fileIO = new FileIO();
-		SettingsIO settingsIO = new SettingsIO(settingsFile);
-		this.settingsIO = settingsIO;
-		
-		// Setup scene, controller, stage
-		primaryStage.setTitle("Profilio - Factorio Profile Manager");
-		
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainWindow.fxml"));
-		MainWindowController controller = new MainWindowController(primaryStage, settingsIO, fileIO, myRegistry);
-		loader.setController(controller);
-		BorderPane root = (BorderPane) loader.load();
-		
-		Scene scene = new Scene(root);
-		//scene.getStylesheets().add("/MainWindowStyle.css");
-		primaryStage.setScene(scene);
-		
-		primaryStage.show();
 	}
 
 	@Override
@@ -111,4 +122,18 @@ public class App extends Application {
 		
 		super.stop();
 	}
+	
+	public void showUncaughtException(Thread t, Throwable e) {
+		try {
+			String errorMsg = String.format("[%s] threw uncaught exception.", t.getName());
+			LOGGER.log(Level.SEVERE, errorMsg, e);
+			Alert alert = new ExceptionDialog(e, errorMsg);
+			alert.showAndWait();
+		} finally {
+			if (! t.isDaemon()) { // Might change this later
+				Platform.exit();
+			}
+		}
+	}
+	
 }
